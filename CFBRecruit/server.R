30c8342a77fd10ff80d606258ca244955eed2163
wcooperreeves <89166343+wcooperreeves@users.noreply.github.com>
2022-01-19 01:16:36 -0500

    shinyDashboard Used
    Used shinydashboard package for an easier to look at application.
    
    - Added Tabs for Map / Charts / Raw Data
    - Download available from Raw Data (both filtered and true raw data)

--- CFBRecruit/server.R
+++ CFBRecruit/server.R
@@ -9,8 +9,13 @@
 
 library(shiny)
 
+CFBData <- read.csv('C:/Users/wcoop/Documents/GitHub/CFB-247-Rvest-Scrape/Player_Data.csv')
+CFBData$Class <- as.character(CFBData$Class)
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+  
+  
 
     output$CollegeChoice <- renderUI({
       selectInput(inputId = 'CollegeChoice',
@@ -20,28 +25,47 @@ shinyServer(function(input, output) {
                   selectize = TRUE)
     })
 
-    output$YearChoice <- renderUI({
-      selectInput(inputId = 'YearChoice',
-                  label = 'Choose Year',
+    output$YearChoiceBegin <- renderUI({
+      selectInput(inputId = 'YearChoiceBegin',
+                  label = 'Choose Start Year',
+                  choices = unique(CFBData$Class),
+                  selected = unique(CFBData$Class)[1],
+                  selectize = TRUE)
+    })
+    
+    
+    output$YearChoiceEnd <- renderUI({
+      selectInput(inputId = 'YearChoiceEnd',
+                  label = 'Choose End Year',
                   choices = unique(CFBData$Class),
                   selected = unique(CFBData$Class)[1],
                   selectize = TRUE)
     })
     
     CFBReactive <- reactive({
-      df <- dplyr::filter(CFBData, CFBData$University %in% input$CollegeChoice, CFBData$Class %in% input$YearChoice)
+      # browser()
+      yearRange <- seq(from = input$YearChoiceBegin, to = input$YearChoiceEnd, by = 1)
+
+      df <- dplyr::filter(CFBData, CFBData$University %in% input$CollegeChoice, CFBData$Class %in% yearRange)
       head(df)
       return(df)
     })
     
     output$CFBFiltered <- DT::renderDataTable({
-      CFBReactive()
+      DT::datatable(CFBReactive(), options = list(pageLength = 30))
     })
     
     output$usmap <- renderLeaflet({
       leaflet() %>%
         addProviderTiles(providers$OpenStreetMap) %>%
-        addMarkers(CFBReactive()$Longitude,CFBReactive()$Latitude, label = as.character(CFBReactive()$Player.Name))
+        addMarkers(CFBReactive()$Longitude,CFBReactive()$Latitude, label = paste0(as.character(CFBReactive()$Player.Name),", ",CFBReactive()$Position,", Star Rating:",CFBReactive()$Star.Rating))
     })
+    
+    output$downloadCSVFilt <- downloadHandler(
+      filename = 'Filtered247Data.csv',
+      content = function(file){
+        write.csv(CFBReactive(), file,row.names = FALSE)
+      }
+    )
 
 })
--- CFBRecruit/ui.R
+++ CFBRecruit/ui.R
@@ -2,23 +2,69 @@ library(shiny)
 library(leaflet)
 library(tidyverse)
 library(DT)
+library(shinydashboard)
 CFBData <- read.csv('C:/Users/wcoop/Documents/GitHub/CFB-247-Rvest-Scrape/Player_Data.csv')
 CFBData$Class <- as.character(CFBData$Class)
 
-shinyUI(fluidPage(
+# shinyUI(fluidPage(
+# 
+#     
+#     header <- dashboardHeader(
+#         title = '247 Recruitment Data'
+#     ),
+#     body <- dashboardBody()
+# 
+#     navbarPage
+#     navbarPage("247 Recruitment Data",
+# 
+#         sidebarPanel(
+# 
+#             uiOutput('CollegeChoice'),
+#             uiOutput('YearChoiceBegin'),
+#             uiOutput('YearChoiceEnd')
+# 
+#         ),
+#         tabPanel("Map",
+# 
+# 
+#                    leafletOutput('usmap')
+# 
+#                  ),
+#         tabPanel("Charts"),
+#         tabPanel("Raw Data",
+#                  DT::dataTableOutput('CFBFiltered')
+# 
+#                  )
+#     )
+#     
+# ))
 
-    # Application title
-    titlePanel("247 Recruitment Data"),
-    
-    
-    
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-      uiOutput('CollegeChoice'),
-      uiOutput('YearChoice'),
+
+dashboardPage(
+    dashboardHeader(title = "247 Recruitment Data"),
+    dashboardSidebar(
+        uiOutput('CollegeChoice'),
+        uiOutput('YearChoiceBegin'),
+        uiOutput('YearChoiceEnd'),
+        sidebarMenu(
+            menuItem('Recruit Map', tabName = 'map'),
+            menuItem('Charts', tabName = 'chart'),
+            menuItem('Dataset', tabName = 'rawdata')
+        )
     ),
-    mainPanel(
-      leafletOutput('usmap'),
-      DT::dataTableOutput('CFBFiltered')
+    dashboardBody(
+        tabItems(
+            tabItem('map',
+                    fluidRow(
+                        leafletOutput('usmap', height = 1000)
+                    )),
+            tabItem('chart'),
+            tabItem('rawdata',
+                    DT::dataTableOutput('CFBFiltered'),
+                    downloadButton('downloadCSVFull',"Download Full Data"),
+                    downloadButton('downloadCSVFilt','Download Displayed Data')
+            )
+        ),
+
     )
-))
+)
